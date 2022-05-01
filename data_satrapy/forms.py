49dcd19f62c102faa06531305e9e884eb3598ffc
OlubiyiMosope <smaolubiyimosope@gmail.com>
2022-05-01 02:28:08 +0100

    Add email and password reset functionality to app.

--- data_satrapy/forms.py
+++ data_satrapy/forms.py
@@ -84,3 +84,22 @@ class UpdateDeleteFieldForm(FlaskForm):
         if not field:
             raise ValidationError("This field does not exist in the database. "
                                   "Please input a field that exists in the database")
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    # To validate that an account exists for the email address whose password is to be reset.
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
--- data_satrapy/routes.py
+++ data_satrapy/routes.py
@@ -1,8 +1,10 @@
-from flask import render_template, url_for, flash, redirect, request, abort
-from data_satrapy import app, db, bcrypt
-from data_satrapy.forms import RegistrationForm, LoginForm, DynamicPostForm, AddFieldForm, UpdateDeleteFieldForm
+from flask import render_template, url_for, flash, redirect, request
+from data_satrapy import app, db, bcrypt, mail
+from data_satrapy.forms import (RegistrationForm, LoginForm, DynamicPostForm, AddFieldForm,
+                                UpdateDeleteFieldForm, RequestResetForm, ResetPasswordForm)
 from data_satrapy.models import User, Post, Field
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 
 CONTENT_COL = 6
@@ -174,3 +176,50 @@ def delete_field(subject_id):
     db.session.delete(field)
     db.session.commit()
     return redirect(url_for("add_field"))
+
+
+# *** RESET EMAIL & PASSWORD ***
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message("Password Reset Request",
+                  sender="noreply@datasatrapy.com",
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form, grid_size=CONTENT_COL_2)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password',
+                           form=form, grid_size=CONTENT_COL_2)
