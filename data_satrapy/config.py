13621f6e6e0f0e43af4431aba5c78d6a99444dab
OlubiyiMosope <smaolubiyimosope@gmail.com>
2022-05-09 19:37:53 +0100

    Changes to files

--- data_satrapy/config.py
+++ data_satrapy/config.py
@@ -1,6 +1,9 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get("FLASK_APP_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///site.db")
