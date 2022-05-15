bc95dc15f4c303c2d3bf564a58258fde958e8689
OlubiyiMosope <smaolubiyimosope@gmail.com>
2022-05-15 19:27:52 +0100

    Edit config

--- satrapy.py
+++ satrapy.py
@@ -1,7 +1,9 @@
 import os
+import click
 from flask_migrate import Migrate, upgrade
 from data_satrapy import create_app, db
 from data_satrapy.models import Post, User, Field
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -10,6 +12,19 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Field=Field, Post=Post)
+
+
+@app.cli.command()
+@click.option('--length', default=25,
+              help='Number of functions to include in the profiler report.')
+@click.option('--profile-dir', default=None,
+              help='Directory where profiler data files are saved.')
+def profile(length, profile_dir):
+    """Start the application under the code profiler."""
+    from werkzeug.middleware.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
 
 
 @app.cli.command()
