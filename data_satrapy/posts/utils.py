b2c26cd4b1faaf1569f63c1ba85ace03ebb12283
OlubiyiMosope <smaolubiyimosope@gmail.com>
2022-05-22 09:07:39 +0100

    edit picture_path variable in post/utils

--- data_satrapy/posts/utils.py
+++ data_satrapy/posts/utils.py
@@ -15,7 +15,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join("app/data_satrapy/", "static/pics", picture_fn)
+    picture_path = os.path.join("/app/data_satrapy/", "static/pics", picture_fn)
 
     output_size = (640, 320)  # (125, 125)
     i = Image.open(form_picture)
